7ca2bbd1e4e61b1057579761753ee1f67b2a172c
André Cavalcanti <andre.cavalcanti.simoes@gmail.com>
2023-06-13 00:16:13 -0300

    Feat: New navbar responsive and create new main content

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
 let btn = document.querySelector("#btn");
 let btnText = document.querySelector("#btnText");
 let btnIcon = document.querySelector("#btnIcon");
+let mobile = document.querySelector(".mobile-btn");
+let navMenu = document.querySelector(".nav-menu");
+
 
 btn.onclick = function(){
     document.body.classList.toggle("dark-theme");
+
     
     if(document.body.classList.contains("dark-theme")) {
-        btnIcon.src = "img/sun.png";
+        btnIcon.classList.remove("fa-moon");
+        btnIcon.classList.add("fa-sun");
         btnText.innerText = "Light";
+       
     }else {
-        btnIcon.src = "img/moon.png";
+        btnIcon.classList.remove("fa-sun");
+        btnIcon.classList.add("fa-moon");
         btnText.innerText = "Dark";
     }
 
 }
 
+mobile.addEventListener("click", () => {
+    mobile.classList.toggle('active');
+    navMenu.classList.toggle('active');
+});
 
